191b341afcda7fddaed43071658521b98f742e0f
Davi <davi.xa.oliveira@gmail.com>
2026-06-23 08:54:24 -0300

    exibe um quadrado sem preenchimento com largura e altura definidas pelo usu

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int largura, altura;
+
+Console.Write("Largura: ");
+largura = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Altura: ");
+altura = Convert.ToInt32(Console.ReadLine());
+
+for (int linha = 1; linha <= altura; linha++)
+{
+    for (int coluna = 1; coluna <= largura; coluna++)
+    {
+        if (linha == 1 || linha == altura)
+        {
+            Console.Write("*");
+        }
+        else if (coluna == 1 || coluna == largura)
+        {
+            Console.Write("*");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+
+    Console.WriteLine(); 
+}
